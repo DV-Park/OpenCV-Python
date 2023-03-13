52f2e8164274c69bb85728fc9cc752cc3ad4d0a1
DV-Park <rhwkdel1@naver.com>
2023-03-13 10:21:42 +0900

    cc

---
+++ main.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+img=cv2.imread('dog.jpg')
+img2=img+50
+img3=cv2.add(img,50)
+img=cv2.vconcat([img,img2,img3])
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
