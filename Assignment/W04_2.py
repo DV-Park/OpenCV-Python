3d8a4ffdaed468d4cbfb4a726487192d3ec67879
DV-Park <rhwkdel1@naver.com>
2023-03-27 11:50:09 +0900

    과제 완

--- Assignment/W04_2.py
+++ Assignment/W04_2.py
@@ -6,13 +6,13 @@ if img is None: raise Exception("영상 파일 읽기 오류")
 
 # 필터의 크기가 커질수록 블러링이 강해진다.
 
-#data =[1/9 for i in range(9)]
+data =[1/9 for i in range(9)]
 #data =[1/25 for i in range(25)]
-data =[1/81 for i in range(81)]
+#data =[1/81 for i in range(81)]
 
-#mask =np.array(data,np.float32).reshape(3,3)
+mask =np.array(data,np.float32).reshape(3,3)
 #mask =np.array(data,np.float32).reshape(5,5)
-mask =np.array(data,np.float32).reshape(9,9)
+#mask =np.array(data,np.float32).reshape(9,9)
 
 blur_img=cv2.filter2D(img,-1,mask)
 dst=cv2.vconcat([img,blur_img])
