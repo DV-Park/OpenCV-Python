65c7ed833d1fce7431f76d4175e2a937edc8af7a
DV-Park <98018435+DV-Park@users.noreply.github.com>
2023-05-27 21:22:43 +0900

    과제

--- Assignment/W10_3_1.py
+++ Assignment/W10_3_1.py
@@ -1,30 +1,38 @@
 import numpy as np
 import cv2
 
-src=cv2.imread("../Src/dft_256.jpg")
-cv2.imshow("Original",src)
+src = cv2.imread("../Src/dft_256.jpg")
+cv2.imshow("Original", src)
 
-t_mat=np.array([[1,0,100],[0,1,-50]], dtype=np.float32)
-dst=cv2.warpAffine(src,t_mat,dsize=(0,0))
+t_mat = np.array([[1, 0, 100], [0, 1, -50]], dtype=np.float32)
+dst = cv2.warpAffine(src, t_mat, dsize=(0, 0))
 cv2.imshow("Translation", dst)
 
-src=cv2.cvtColor(src,cv2.COLOR_BGR2GRAY)
-dst=cv2.cvtColor(dst,cv2.COLOR_BGR2GRAY)
+src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+dst_gray = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
 
-src=np.float32(src)
-dst=np.float32(dst)
-ret=cv2.phaseCorrelate(src,dst)
+src_gray = np.float32(src_gray)
+dst_gray = np.float32(dst_gray)
+ret = cv2.phaseCorrelate(src_gray, dst_gray)
 print(ret)
 
-#추가한것
-fft_src=np.fft.fft(src)
-fft_dst=np.fft.fft(dst)
-fft_dst_conj=np.conj(fft_dst)
+# 위상 상관 결과에서 이동 거리 계산
+shift_x, shift_y = ret[0]
+rows, cols = src_gray.shape
+dx = shift_x if shift_x <= rows / 2 else shift_x - rows
+dy = shift_y if shift_y <= cols / 2 else shift_y - cols
+print("이동 거리 (dx, dy):", (dx, dy))
 
-R=(fft_src*fft_dst_conj)/abs(fft_src*fft_dst_conj)
-r=np.fft.ifft(R)
+# 추가한 부분
+fft_src = np.fft.fft2(src_gray)
+fft_dst = np.fft.fft2(dst_gray)
+fft_dst_conj = np.conj(fft_dst)
+
+R = (fft_src * fft_dst_conj) / np.abs(fft_src * fft_dst_conj)
+r = np.fft.ifft2(R)
+
+time_shift = np.argmax(r)
+print("시간 이동 거리:", time_shift)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-time_shift=np.argmax(r)
-print("Time shift=%d" % (time_shift))
--- Assignment/W10_4.py
+++ Assignment/W10_4.py
@@ -1,12 +1,41 @@
-import numpy as np, cv2, math
-import scipy.fftpack as sf
+import numpy as np, cv2
+from SrcCode.Common.dct2d import *
 
-block=np.zeros((8,8),np.uint8)
-cv2.randn(block,128,50)
+def dct2_mode(block, mode):
+    if mode==1: return cv2.dct(block.astype('float32'))
+    elif mode==2: return  cv2.dct(block.astype('float64'))
 
-dct=cv2.dct(block.astype("float32"))
-idct=cv2.dct(dct,flags=cv2.DCT_INVERSE)
+def idct2_mode(block, mode):
+    if mode==1: return cv2.idct(block.astype('float32'))
+    elif mode==2: return cv2.idct(block.astype('float64'))
 
-print("Original block=\n", block)
-print("dct(OpenCV 함수)=\n", dct)
-print("idct(OpenCV 함수)=\n", cv2.convertScaleAbs(idct))
+
+def dct_filtering(img, filter, M, N):
+    dst = np.empty(img.shape, np.float32)
+    for i in range(0, img.shape[0], M):
+        for j in range(0, img.shape[1], N):
+            block = img[i:i+M, j:j+N]
+            dct_block = dct2_mode(block, mode)
+            dct_block = dct_block * filter
+            dst[i:i+M, j:j+N] = idct2_mode(dct_block, mode)
+    return cv2.convertScaleAbs(dst)
+
+image = cv2.imread("../Src/dct.jpg", cv2.IMREAD_GRAYSCALE)
+if image is None: raise Exception("영상 파일을 읽기 에러")
+mode = 2
+M, N = 8, 8
+filters = [np.zeros((M, N), np.float32) for i in range(5)]
+titles = ['DC Pass', 'High Pass', 'Low Pass', 'Vertical Pass', 'Horizental Pass' ]
+
+filters[0][0, 0] = 1                        # DC 계수만 1 지정 – DC Pass
+filters[1][:], filters[1][0, 0] = 1, 0      # 모든 계수 1, DC 계수만 0 지정 – High Pass
+filters[2][:M//2, :N//2] = 1            # 저주파 영역 모두 1 지정 – Low Pass
+filters[3][0, 1:] = 1                        # 첫 행열 1 지정 – Vertical
+filters[4][1:, 0] = 1                  # 첫 열만 1 지정 – Horizental
+
+for filter, title in zip(filters, titles):
+    dst = dct_filtering(image, filter, M, N)          # ?? ??? DCT ??
+    cv2.imshow(title, dst)
+
+cv2.imshow("image", image)
+cv2.waitKey(0)
